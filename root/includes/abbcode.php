fea447778cbbe0c65e0515623bc4231b27ec8c98
Gabriel <forum@mssti.com>
2010-07-26 13:51:30 -0300

    Some tiny changes

--- root/includes/abbcode.php
+++ root/includes/abbcode.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * @package: phpBB 3.0.8 :: Advanced BBCode box 3 -> root/includes
-* @version: $Id: abbcode.php, v 3.0.8 2010/07/22 10:07:22 leviatan21 Exp $
+* @version: $Id: abbcode.php, v 3.0.8 2010/07/26 10:07:26 leviatan21 Exp $
 * @copyright: leviatan21 < info@mssti.com > (Gabriel) http://www.mssti.com/phpbb3/
 * @license: http://opensource.org/licenses/gpl-license.php GNU Public License 
 * @author: leviatan21 - http://www.phpbb.com/community/memberlist.php?mode=viewprofile&u=345763
