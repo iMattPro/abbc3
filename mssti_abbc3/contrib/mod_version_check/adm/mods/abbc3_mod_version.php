1cac3c96485e5f8bd9b0eb0c6542cf9f115ddda6
Matt Friedman <maf675@gmail.com>
2011-08-06 18:47:26 -0700

    Fix 1 comma, and increment everything up to 3.0.9.2 :(

--- mssti_abbc3/contrib/mod_version_check/adm/mods/abbc3_mod_version.php
+++ mssti_abbc3/contrib/mod_version_check/adm/mods/abbc3_mod_version.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * @package: phpBB 3.0.9 :: Advanced BBCode box 3 mod version check -> root/adm/mods
-* @version: $Id: abbc3_mod_version.php, v 3.0.9.1 05/12/11 12:22 PM leviatan21 Exp $
+* @version: $Id: abbc3_mod_version.php, v 3.0.9.2 05/12/11 12:22 PM leviatan21 Exp $
 * @copyright: leviatan21 < info@mssti.com > (Gabriel) http://www.mssti.com/phpbb3/
 * @license: http://opensource.org/licenses/gpl-license.php GNU Public License
 * @author: leviatan21 - http://www.phpbb.com/community/memberlist.php?mode=viewprofile&u=345763
@@ -24,7 +24,7 @@ class abbc3_mod_version
 			'author'	=> 'leviatan21',
 			'title'		=> 'Advanced BBCode Box 3 (aka ABBC3)',
 			'tag'		=> 'abbc3',
-			'version'	=> '3.0.9.1',
+			'version'	=> '3.0.9.2',
 			'file'		=> array('mssti.com', 'phpbb3', 'mssti_phpbb3x_mods.xml'),
 		);
 	}
