f4570ff9205b6a67deeae94222a7618d2995a55f
Matt Friedman <maf675@gmail.com>
2012-08-30 18:46:48 -0700

    Add prettyPhoto license

--- mssti_abbc3/root/styles/abbcode/prettyPhoto/js/jquery.prettyPhoto.js
+++ mssti_abbc3/root/styles/abbcode/prettyPhoto/js/jquery.prettyPhoto.js
@@ -3,7 +3,6 @@
 	Use: Lightbox clone for jQuery
 	Author: Stephane Caron (http://www.no-margin-for-errors.com)
 	Version: 3.1.4
-	prettyPhoto is totally free to use, it is released under the GPLv2 license (http://www.gnu.org/licenses/gpl-2.0.html)
 ------------------------------------------------------------------------- */
 
 (function($){$.prettyPhoto={version:'3.1.4'};$.fn.prettyPhoto=function(pp_settings){pp_settings=jQuery.extend({hook:'rel',animation_speed:'fast',ajaxcallback:function(){},slideshow:5000,autoplay_slideshow:false,opacity:0.80,show_title:true,allow_resize:true,allow_expand:true,default_width:500,default_height:344,counter_separator_label:'/',theme:'pp_default',horizontal_padding:20,hideflash:false,wmode:'opaque',autoplay:true,modal:false,deeplinking:true,overlay_gallery:true,overlay_gallery_max:30,keyboard_shortcuts:true,changepicturecallback:function(){},callback:function(){},ie6_fallback:true,markup:'<div class="pp_pic_holder"> \
