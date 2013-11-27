0b4e9393a4ed1bd53bcdd2f4507b26b097ce7bb3
Matt Friedman <maf675@gmail.com>
2013-11-27 12:42:50 -0800

    Localise helpline tags into lang files

--- migrations/v310_update_data.php
+++ migrations/v310_update_data.php
@@ -109,115 +109,115 @@ class v310_update_data extends \phpbb\db\migration\migration
 	public function abbc3_bbcode_data()
 	{
 		return array(
-			'BBvideo' => array(
-				'bbcode_helpline'	=> '[BBvideo=width,height]Video URL[/BBvideo]',
-				'bbcode_match'		=> '[BBvideo={NUMBER1},{NUMBER2}]{URL}[/BBvideo]',
-				'bbcode_tpl'		=> '<a href="{URL}" class="bbvideo" data-bbvideo="{NUMBER1},{NUMBER2}">{URL}</a>',
-			),
-			'glow=' => array(
-				'bbcode_helpline'	=> 'Glow text: [glow=color]text[/glow]',
-				'bbcode_match'		=> '[glow={COLOR}]{TEXT}[/glow]',
-				'bbcode_tpl'		=> '<span class="glow" style="display: inline-block; padding: 0 0.5em; color: #ffffff; text-shadow: 0 0 1em {COLOR}, 0 0 1em {COLOR}, 0 0 1.2em {COLOR};">{TEXT}</span>',
-			),
-			'shadow=' => array(
-				'bbcode_helpline'	=> 'Shadow text: [shadow=color]text[/shadow]',
-				'bbcode_match'		=> '[shadow={COLOR}]{TEXT}[/shadow]',
-				'bbcode_tpl'		=> '<span class="shadow" style="display: inline-block; padding: 0 0.5em; color: {COLOR}; text-shadow: -0.2em 0.2em 0.2em #999;">{TEXT}</span>',
-			),
-			'dropshadow=' => array(
-				'bbcode_helpline'	=> 'Drop shadow text: [dropshadow=color]text[/dropshadow]',
-				'bbcode_match'		=> '[dropshadow={COLOR}]{TEXT}[/dropshadow]',
-				'bbcode_tpl'		=> '<span class="dropshadow" style="display: inline-block; padding: 0 0.5em; color: {COLOR}; text-shadow: -0.1em 0.1em 0.05em #999;">{TEXT}</span>',
-			),
-			'blur=' => array(
-				'bbcode_helpline'	=> 'Blur text: [blur=color]text[/blur]',
-				'bbcode_match'		=> '[blur={COLOR}]{TEXT}[/blur]',
-				'bbcode_tpl'		=> '<span class="blur" style="display: inline-block; padding: 0 0.5em; color: transparent; text-shadow: 0 0 0.2em {COLOR};">{TEXT}</span>',
-			),
-			'fade' => array(
-				'bbcode_helpline'	=> 'Fade text: [fade]text[/fade]',
-				'bbcode_match'		=> '[fade]{TEXT}[/fade]',
-				'bbcode_tpl'		=> '<span class="fadeEffect">{TEXT}</span>',
-			),
-			'mod=' => array(
-				'bbcode_helpline'	=> 'Alert text: [mod=username]text[/mod]',
-				'bbcode_match'		=> '[mod={TEXT1}]{TEXT2}[/mod]',
-				'bbcode_tpl'		=> '<table class="ModTable"><tr><td class="exclamation" rowspan="2">&nbsp;!&nbsp;</td><td class="rowuser">{TEXT1} {L_MESSAGE}:</td></tr><tr><td class="rowtext">{TEXT2}</td></tr></table>',
-			),
-			'spoil' => array(
-				'bbcode_helpline'	=> 'Spoiler text: [spoil]text[/spoil]',
-				'bbcode_match'		=> '[spoil]{TEXT}[/spoil]',
-				'bbcode_tpl'		=> '<div class="spoilwrapper"><div class="spoiltitle"><span class="btnspoil" data-show="{L_SPOILER_SHOW}" data-hide="{L_SPOILER_HIDE}">{L_SPOILER_SHOW}</span></div><div style="display: none;" class="spoilcontent">{TEXT}</div></div>',
-			),
-			'nfo' => array(
-				'bbcode_helpline'	=> 'NFO ascii art text: [nfo]NFO text[/nfo]',
-				'bbcode_match'		=> '[nfo]{TEXT}[/nfo]',
-				'bbcode_tpl'		=> '<pre class="nfo">{TEXT}</pre>',
-			),
-			'offtopic' => array(
-				'bbcode_helpline'	=> 'Off topic text: [offtopic]text[/offtopic]',
-				'bbcode_match'		=> '[offtopic]{TEXT}[/offtopic]',
-				'bbcode_tpl'		=> '<div class="OffTopic"><div class="OffTopicTitle">{L_OFFTOPIC}</div><div class="OffTopicText">{TEXT}</div></div>',
-			),
-			'hidden' => array(
-				'bbcode_helpline'	=> 'Hide text from guests: [hidden]text[/hidden]',
-				'bbcode_match'		=> '[hidden]{TEXT}[/hidden]',
-				'bbcode_tpl'		=> '<!-- ABBC3_BBCODE_HIDDEN -->{TEXT}<!-- ABBC3_BBCODE_HIDDEN -->',
-			),
-			'align=center' => array(
-				'bbcode_helpline'	=> 'Align text: [align=center|left|right|justify]text[/align]',
-				'bbcode_match'		=> '[align={SIMPLETEXT}]{TEXT}[/align]',
-				'bbcode_tpl'		=> '<span style="text-align: {SIMPLETEXT}; display: block;">{TEXT}</span>',
-			),
-			'dir=ltr' => array(
-				'bbcode_helpline'	=> 'Text direction: [dir=ltr|rtl]text[/dir]',
-				'bbcode_match'		=> '[dir={SIMPLETEXT}]{TEXT}[/dir]',
-				'bbcode_tpl'		=> '<bdo dir="{SIMPLETEXT}">{TEXT}</bdo>',
-			),
-			'marq=up' => array(
-				'bbcode_helpline'	=> 'Marquee text: [marq=up|down|left|right]text[/marq]',
-				'bbcode_match'		=> '[marq={SIMPLETEXT}]{TEXT}[/marq]',
-				'bbcode_tpl'		=> '<marquee direction="{SIMPLETEXT}" scrolldelay="100" onmouseover="this.scrollDelay=10000000;" onmouseout="this.scrollDelay=100;">{TEXT}</marquee>',
-			),
 			'font=' => array(
-				'bbcode_helpline'	=> 'Font Type: [font=Comic Sans MS]text[/font]',
+				'bbcode_helpline'	=> 'ABBC3_FONT_HELPLINE',
 				'bbcode_match'		=> '[font={INTTEXT}]{TEXT}[/font]',
 				'bbcode_tpl'		=> '<span style="font-family: {INTTEXT};">{TEXT}</span>',
 			),
 			'highlight=' => array(
-				'bbcode_helpline'	=> 'Highlight text: [highlight=yellow]text[/highlight]',
+				'bbcode_helpline'	=> 'ABBC3_HIGHLIGHT_HELPLINE',
 				'bbcode_match'		=> '[highlight={COLOR}]{TEXT}[/highlight]',
 				'bbcode_tpl'		=> '<span style="background-color: {COLOR};">{TEXT}</span>',
 			),
+			'align=center' => array(
+				'bbcode_helpline'	=> 'ABBC3_ALIGN_HELPLINE',
+				'bbcode_match'		=> '[align={SIMPLETEXT}]{TEXT}[/align]',
+				'bbcode_tpl'		=> '<span style="text-align: {SIMPLETEXT}; display: block;">{TEXT}</span>',
+			),
+			'pre' => array(
+				'bbcode_helpline'	=> 'ABBC3_PREFORMAT_HELPLINE',
+				'bbcode_match'		=> '[pre]{TEXT}[/pre]',
+				'bbcode_tpl'		=> '<pre>{TEXT}</pre>',
+			),
 			's' => array(
-				'bbcode_helpline'	=> 'Strike-through text: [s]text[/s]',
+				'bbcode_helpline'	=> 'ABBC3_STRIKE_HELPLINE',
 				'bbcode_match'		=> '[s]{TEXT}[/s]',
 				'bbcode_tpl'		=> '<span style="text-decoration: line-through;">{TEXT}</span>',
 			),
 			'sup' => array(
-				'bbcode_helpline'	=> 'Superscript text: [sup]text[/sup]',
+				'bbcode_helpline'	=> 'ABBC3_SUP_HELPLINE',
 				'bbcode_match'		=> '[sup]{TEXT}[/sup]',
 				'bbcode_tpl'		=> '<sup>{TEXT}</sup>',
 			),
 			'sub' => array(
-				'bbcode_helpline'	=> 'Subscript text: [sub]text[/sub]',
+				'bbcode_helpline'	=> 'ABBC3_SUB_HELPLINE',
 				'bbcode_match'		=> '[sub]{TEXT}[/sub]',
 				'bbcode_tpl'		=> '<sub>{TEXT}</sub>',
 			),
-			'pre' => array(
-				'bbcode_helpline'	=> 'Preformatted text: [pre]text[/pre]',
-				'bbcode_match'		=> '[pre]{TEXT}[/pre]',
-				'bbcode_tpl'		=> '<pre>{TEXT}</pre>',
+			'glow=' => array(
+				'bbcode_helpline'	=> 'ABBC3_GLOW_HELPLINE',
+				'bbcode_match'		=> '[glow={COLOR}]{TEXT}[/glow]',
+				'bbcode_tpl'		=> '<span class="glow" style="display: inline-block; padding: 0 0.5em; color: #ffffff; text-shadow: 0 0 1em {COLOR}, 0 0 1em {COLOR}, 0 0 1.2em {COLOR};">{TEXT}</span>',
 			),
-			'youtube' => array(
-				'bbcode_helpline'	=> 'YouTube Video: [youtube]URL[/youtube]',
-				'bbcode_match'		=> '[youtube]{URL}[/youtube]',
-				'bbcode_tpl'		=> '<a href="{URL}" class="bbvideo" data-bbvideo="560,340">{URL}</a>',
+			'shadow=' => array(
+				'bbcode_helpline'	=> 'ABBC3_SHADOW_HELPLINE',
+				'bbcode_match'		=> '[shadow={COLOR}]{TEXT}[/shadow]',
+				'bbcode_tpl'		=> '<span class="shadow" style="display: inline-block; padding: 0 0.5em; color: {COLOR}; text-shadow: -0.2em 0.2em 0.2em #999;">{TEXT}</span>',
+			),
+			'dropshadow=' => array(
+				'bbcode_helpline'	=> 'ABBC3_DROPSHADOW_HELPLINE',
+				'bbcode_match'		=> '[dropshadow={COLOR}]{TEXT}[/dropshadow]',
+				'bbcode_tpl'		=> '<span class="dropshadow" style="display: inline-block; padding: 0 0.5em; color: {COLOR}; text-shadow: -0.1em 0.1em 0.05em #999;">{TEXT}</span>',
+			),
+			'blur=' => array(
+				'bbcode_helpline'	=> 'ABBC3_BLUR_HELPLINE',
+				'bbcode_match'		=> '[blur={COLOR}]{TEXT}[/blur]',
+				'bbcode_tpl'		=> '<span class="blur" style="display: inline-block; padding: 0 0.5em; color: transparent; text-shadow: 0 0 0.2em {COLOR};">{TEXT}</span>',
+			),
+			'fade' => array(
+				'bbcode_helpline'	=> 'ABBC3_FADE_HELPLINE',
+				'bbcode_match'		=> '[fade]{TEXT}[/fade]',
+				'bbcode_tpl'		=> '<span class="fadeEffect">{TEXT}</span>',
+			),
+			'dir=ltr' => array(
+				'bbcode_helpline'	=> 'ABBC3_DIR_HELPLINE',
+				'bbcode_match'		=> '[dir={SIMPLETEXT}]{TEXT}[/dir]',
+				'bbcode_tpl'		=> '<bdo dir="{SIMPLETEXT}">{TEXT}</bdo>',
+			),
+			'marq=up' => array(
+				'bbcode_helpline'	=> 'ABBC3_MARQUEE_HELPLINE',
+				'bbcode_match'		=> '[marq={SIMPLETEXT}]{TEXT}[/marq]',
+				'bbcode_tpl'		=> '<marquee direction="{SIMPLETEXT}" scrolldelay="100" onmouseover="this.scrollDelay=10000000;" onmouseout="this.scrollDelay=100;">{TEXT}</marquee>',
+			),
+			'spoil' => array(
+				'bbcode_helpline'	=> 'ABBC3_SPOILER_HELPLINE',
+				'bbcode_match'		=> '[spoil]{TEXT}[/spoil]',
+				'bbcode_tpl'		=> '<div class="spoilwrapper"><div class="spoiltitle"><span class="btnspoil" data-show="{L_ABBC3_SPOILER_SHOW}" data-hide="{L_ABBC3_SPOILER_HIDE}">{L_ABBC3_SPOILER_SHOW}</span></div><div style="display: none;" class="spoilcontent">{TEXT}</div></div>',
+			),
+			'hidden' => array(
+				'bbcode_helpline'	=> 'ABBC3_HIDDEN_HELPLINE',
+				'bbcode_match'		=> '[hidden]{TEXT}[/hidden]',
+				'bbcode_tpl'		=> '<!-- ABBC3_BBCODE_HIDDEN -->{TEXT}<!-- ABBC3_BBCODE_HIDDEN -->',
+			),
+			'offtopic' => array(
+				'bbcode_helpline'	=> 'ABBC3_OFFTOPIC_HELPLINE',
+				'bbcode_match'		=> '[offtopic]{TEXT}[/offtopic]',
+				'bbcode_tpl'		=> '<div class="OffTopic"><div class="OffTopicTitle">{L_ABBC3_OFFTOPIC}</div><div class="OffTopicText">{TEXT}</div></div>',
+			),
+			'mod=' => array(
+				'bbcode_helpline'	=> 'ABBC3_MOD_HELPLINE',
+				'bbcode_match'		=> '[mod={TEXT1}]{TEXT2}[/mod]',
+				'bbcode_tpl'		=> '<table class="ModTable"><tr><td class="exclamation" rowspan="2">&nbsp;!&nbsp;</td><td class="rowuser">{TEXT1} {L_MESSAGE}:</td></tr><tr><td class="rowtext">{TEXT2}</td></tr></table>',
+			),
+			'nfo' => array(
+				'bbcode_helpline'	=> 'ABBC3_NFO_HELPLINE',
+				'bbcode_match'		=> '[nfo]{TEXT}[/nfo]',
+				'bbcode_tpl'		=> '<pre class="nfo">{TEXT}</pre>',
 			),
 			'soundcloud' => array(
-				'bbcode_helpline'	=> '[soundcloud]http://soundcloud.com/user-name/song-title[/soundcloud]',
+				'bbcode_helpline'	=> 'ABBC3_SOUNDCLOUD_HELPLINE',
 				'bbcode_match'		=> '[soundcloud]{URL}[/soundcloud]',
 				'bbcode_tpl'		=> '<object height="81" width="100%"><param name="movie" value="http://player.soundcloud.com/player.swf?url={URL}&amp;g=bb"></param><param name="allowscriptaccess" value="always"></param><embed allowscriptaccess="always" height="81" src="http://player.soundcloud.com/player.swf?url={URL}&amp;g=bb" type="application/x-shockwave-flash" width="100%"></embed></object>',
+			),
+			'BBvideo' => array(
+				'bbcode_helpline'	=> 'ABBC3_BBVIDEO_HELPLINE',
+				'bbcode_match'		=> '[BBvideo={NUMBER1},{NUMBER2}]{URL}[/BBvideo]',
+				'bbcode_tpl'		=> '<a href="{URL}" class="bbvideo" data-bbvideo="{NUMBER1},{NUMBER2}">{URL}</a>',
+			),
+			'youtube' => array(
+				'bbcode_helpline'	=> 'ABBC3_YOUTUBE_HELPLINE',
+				'bbcode_match'		=> '[youtube]{URL}[/youtube]',
+				'bbcode_tpl'		=> '<a href="{URL}" class="bbvideo" data-bbvideo="560,340">{URL}</a>',
 			),
 		);
 	}
