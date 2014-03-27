ffb284f32bd0ff47c8f3b83a9427cc20e150d857
Matt Friedman <maf675@gmail.com>
2014-03-26 22:50:34 -0700

    Store BBvideo example data in external JSON file

--- controller/wizard.php
+++ controller/wizard.php
@@ -26,6 +26,9 @@ class wizard
 	/** @var \phpbb\user */
 	protected $user;
 
+	/** @var string phpBB root path */
+	protected $root_path;
+
 	/**
 	* Constructor
 	*
@@ -33,15 +36,17 @@ class wizard
 	* @param \phpbb\request\request      $request     Request object
 	* @param \phpbb\template\template    $template    Template object
 	* @param \phpbb\user                 $user        User object
+	* @param $root_path
 	* @return \vse\abbc3\controller\wizard
 	* @access public
 	*/
-	public function __construct(\phpbb\controller\helper $helper, \phpbb\request\request $request, \phpbb\template\template $template, \phpbb\user $user)
+	public function __construct(\phpbb\controller\helper $helper, \phpbb\request\request $request, \phpbb\template\template $template, \phpbb\user $user, $root_path)
 	{
 		$this->helper = $helper;
 		$this->request = $request;
 		$this->template = $template;
 		$this->user = $user;
+		$this->root_path = $root_path;
 	}
 
 	/**
@@ -100,82 +105,14 @@ class wizard
 	}
 
 	/**
-	* Return an array of allowed BBvideo sites and example URLs
+	* Return an array of allowed BBvideo sites and example URLs (stored in bbvideo.json)
 	*
 	* @return array Allowed BBvideo sites and URLs
 	* @access private
 	*/
 	private function bbvideo_sites()
 	{
-		return array(
-			'5min.com' => 'http://www.5min.com/Video/iPad-to-Embrace-New-Name-517297508',
-			'allocine.fr' => 'http://www.allocine.fr/video/player_gen_cmedia=19149857&cfilm=126693.html',
-			'blip.tv' => 'http://blip.tv/disenchanted/disenchanted-ep-101-once-upon-a-crackhouse-6063266',
-			'break.com' => 'http://www.break.com/index/wakeboarding-boss-level-2315925',
-			'clipfish.de' => 'http://www.clipfish.de/video/1856437/ac-dc-tnt/',
-			'clipmoon.com' => 'http://www.clipmoon.com/videos/9194d9/animation-versus-animator.html',
-			'cnbc.com' => 'http://video.cnbc.com/gallery/?video=1548022077&play=1',
-			'cnettv.cnet.com' => 'http://cnettv.cnet.com/kinect-controlled-motorized-skateboard/9742-1_53-50118306.html',
-			'colbertnation.com' => 'http://www.colbertnation.com/the-colbert-report-videos/180900/october-17-2005/intro---10-17-05',
-			'collegehumor.com' => 'http://www.collegehumor.com/video/6747386/skyrim-hoarders',
-			'comedycentral.com' => 'http://www.comedycentral.com/video-clips/l56fcp/futurama-trashy-robo-sluts',
-			'crackle.com' => 'http://www.crackle.com/c/The_Walking_Dead/TS-19/2483060/',
-			'dailymotion.com' => 'http://www.dailymotion.com/video/x4ez1x_alberto-contra-el-heliocentrismo_sport',
-			'dotsub.com' => 'http://dotsub.com/view/6a7db231-4d64-407d-8026-a845eaf6c4a9',
-			'ebaumsworld.com' => 'http://www.ebaumsworld.com/video/watch/82424906/',
-			'facebook.com' => 'https://www.facebook.com/photo.php?v=2031763147233',
-			'flickr.com' => 'http://www.flickr.com/photos/chrismar/3071009125',
-			'funnyordie.com' => 'http://www.funnyordie.com/videos/5ef1adb57b/between-two-ferns-with-zach-galifianakis',
-			'g4tv.com' => 'http://g4tv.com/videos/29265/Infamous-All-Access/',
-			'gameprotv.com' => 'http://www.gameprotv.com/socom-4-us-navy-seals-trailer-video-6923.html',
-			'gamespot.com' => 'http://www.gamespot.com/video/928334/6185856/lost-odyssey-official-trailer-8',
-			'gametrailers.com' => 'http://www.gametrailers.com/videos/j3gx9q/facebreaker-world-premiere-exclusive-debut',
-			'gamevideos.1up.com' => 'http://gamevideos.1up.com/video/id/17766',
-			'godtube.com' => 'http://www.godtube.com/watch/?v=9JJBE1NU',
-			'howcast.com' => 'http://www.howcast.com/videos/499089-How-to-Draw-Manga-How-to-Draw-Monsters',
-			'hulu.com' => 'http://www.hulu.com/watch/308687/the-office-dwight-vs-jim-prank-tacular',
-			'ign.com' => 'http://www.ign.com/videos/2012/04/05/double-dragon-neon-gameplay-trailer',
-			'liveleak.com' => 'http://www.liveleak.com/view?i=166_1194290849',
-			'metacafe.com' => 'http://www.metacafe.com/watch/966360/merry_christmas_with_crazy_frog/',
-			'moddb.com' => 'http://www.moddb.com/groups/humour-satire-parody/videos/flight-dc132-part-1',
-			'mpora.com' => 'http://mpora.com/videos/AAdihftgw4t7',
-			'myspace.com' => 'https://myspace.com/jasonmraz/video/-quot-lucky-quot-official-video-with-colbie-caillat/49776296',
-			'myvideo.de' => 'http://www.myvideo.de/watch/2668372',
-			'nbcnews.com' => 'http://www.nbcnews.com/video/nbc-news/53875621/#53875621',
-			'on.aol.com' => 'http://on.aol.com/video/ipad-to-embrace-new-name-517297508',
-			'photobucket.com' => 'http://s0006.photobucket.com/albums/0006/pbhomepage/Ice%20Age/?action=view&current=TFEIT301100-H264_Oct27.mp4',
-			'rutube.ru' => 'http://rutube.ru/video/238973b0c167d0a9f4f26686e42407e4/',
-			'sapo.pt' => 'http://videos.sapo.pt/LguPabwSWikK0wzBmU1o',
-			'screenr.com' => 'http://www.screenr.com/fTK',
-			'scribd.com' => 'http://www.scribd.com/doc/26886617/Dexter-Investigating-Cutting-Edge-Television',
-			'sevenload.com' => 'http://www.sevenload.com/videos/moskovskii-most-po-vantam-5129e95932b0c28c55000079',
-			'slideshare.net' => 'http://www.slideshare.net/chrisbrogan/social-media-for-publishers-presentation',
-			'snotr.com' => 'http://www.snotr.com/video/8753/What_is_nothing',
-			'spike.com' => 'http://www.spike.com/video-clips/32xg36/winter-passing-trailer',
-			'streetfire.net' => 'http://www.streetfire.net/video/standing-moto-double-fail_2381106.htm',
-			'ted.com' => 'http://www.ted.com/talks/henry_evans_and_chad_jenkins_meet_the_robots_for_humanity.html',
-			'thedailyshow.com' => 'http://www.thedailyshow.com/watch/thu-june-28-2012/roberts--rules-of-order',
-			'theonion.com' => 'http://www.theonion.com/video/stephen-strasburg-ceremoniously-reinjures-arm-on-o,27866/',
-			'tudou.com' => 'http://www.tudou.com/programs/view/fP78-keWP6c/',
-			'twitch.tv' => 'http://www.twitch.tv/rzn732',
-			'twitvid.com' => 'http://twitvid.com/0U73M',
-			'ustream.tv' => 'http://www.ustream.tv/channel/9948292',
-			'vbox7.com' => 'http://www.vbox7.com/play:93ab2ba5',
-			'veoh.com' => 'http://www.veoh.com/watch/v27458670er62wkCt',
-			'vevo.com' => 'http://www.vevo.com/watch/USUV71300904',
-			'viddler.com' => 'http://www.viddler.com/v/7a0d64f2',
-			'videogamer.com' => 'http://www.videogamer.com/videos/dead_space_developer_diary_zero_gravity.html',
-			'videu.de' => 'http://www.videu.de/video/38',
-			'vimeo.com' => 'http://vimeo.com/3759030',
-			'vine.co' => 'https://vine.co/v/hBFxTlV36Tg',
-			'wat.tv' => 'http://www.wat.tv/video/mords-moi-sans-hesitation-2ykhj_2g5h3_.html',
-			'wegame.com' => 'http://www.wegame.com/watch/Clarity_Darkspear_VS_Heigan/',
-			'xfire.com' => 'http://www.xfire.com/video/24c86/',
-			'yahoo.com' => 'http://screen.yahoo.com/man-steel-trailer-5-163029535.html',
-			'youku.com' => 'http://v.youku.com/v_show/id_XMzgxNzY3NTU2.html',
-			'youtu.be' => 'http://youtu.be/sP4NMoJcFd4',
-			'youtube.com' => 'http://www.youtube.com/watch?v=sP4NMoJcFd4',
-		);
+		return json_decode(file_get_contents($this->root_path . 'ext/vse/abbc3/assets/bbvideo.json'), true);
 	}
 
 	/**
