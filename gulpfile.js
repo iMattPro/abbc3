b175f1e1752bd68735ae99d39d076e5061496564
Matt Friedman <maf675@gmail.com>
2014-05-25 10:57:48 -0700

    Add Gulp task runner

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp'),
+	minifycss = require('gulp-minify-css'),
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify'),
+	jshint = require('gulp-jshint'),
+	csslint = require('gulp-csslint'),
+	stylish = require('jshint-stylish'),
+	paths = {
+		css: ['styles/all/theme/*.css', '!styles/all/theme/*.min.css'],
+		js: ['styles/all/template/js/*.js', '!styles/all/template/js/*.min.js'],
+	};
+
+// Lint JS
+gulp.task('jshint', function() {
+	return gulp.src(paths.js)
+		.pipe(jshint())
+		.pipe(jshint.reporter(stylish));
+});
+
+// Minify JS
+gulp.task('js', function() {
+	return gulp.src(paths.js)
+		.pipe(rename({suffix: '.min'} ))
+		.pipe(uglify())
+		.pipe(gulp.dest('styles/all/template/js/'));
+});
+
+// Lint CSS
+gulp.task('csslint', function() {
+	return gulp.src(paths.css)
+		.pipe(csslint({
+			'ids': false,
+			'important': false,
+			'box-model': false,
+		}))
+		.pipe(csslint.reporter());
+});
+
+// Minify CSS
+gulp.task('css', function() {
+	return gulp.src(paths.css)
+		.pipe(rename({suffix: '.min'} ))
+		.pipe(minifycss())
+		.pipe(gulp.dest('styles/all/theme/'));
+});
+
+// Watch CSS and JS files with $ gulp watch
+gulp.task('watch', function() {
+	gulp.watch(paths.js, ['js']);
+	gulp.watch(paths.css, ['css']);
+});
+
+// Run linting tasks with $ gulp lint
+gulp.task('lint', ['jshint', 'csslint']);
+
+// Run default tasks with $ gulp
+gulp.task('default', ['js', 'css']);
