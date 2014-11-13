3c1d76b320ada80586b8b8af5b45b025e22fc3b7
Matt Friedman <maf675@gmail.com>
2014-11-12 21:33:08 -0800

    Move bbcode install function into a class

---
+++ core/bbcodes_manager.php
@@ -0,0 +1,144 @@
+<?php
+/**
+*
+* Advanced BBCode Box 3.1
+*
+* @copyright (c) 2014 Matt Friedman
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace vse\abbc3\core;
+
+/**
+* ABBC3 BBCodes manager class
+*/
+class bbcodes_manager
+{
+	/** @var \phpbb\db\driver\driver_interface */
+	protected $db;
+
+	/** @var \phpbb\request\request */
+	protected $request;
+
+	/** @var \phpbb\user */
+	protected $user;
+
+	/** @var string */
+	protected $table_prefix;
+
+	/** @var string */
+	protected $phpbb_root_path;
+
+	/** @var string */
+	protected $php_ext;
+
+	/**
+	* Constructor
+	*
+	* @param \phpbb\db\driver\driver_interface $db
+	* @param \phpbb\request\request $request
+	* @param \phpbb\user $user
+	* @param string $phpbb_root_path
+	* @param string $php_ext
+	* @param string $table_prefix
+	* @return \vse\abbc3\core\bbcodes_manager
+	* @access public
+	*/
+	public function __construct(\phpbb\db\driver\driver_interface $db, \phpbb\request\request $request, \phpbb\user $user, $phpbb_root_path, $php_ext, $table_prefix)
+	{
+		$this->db = $db;
+		$this->request = $request;
+		$this->user = $user;
+
+		$this->phpbb_root_path = $phpbb_root_path;
+		$this->php_ext = $php_ext;
+		$this->table_prefix = $table_prefix;
+	}
+
+	/**
+	* Installs BBCodes, used by migrations to perform add/updates
+	*
+	* @param array $bbcode_data Array of BBCode data to install
+	* @return null
+	* @access public
+	*/
+	public function install_bbcodes($bbcode_data)
+	{
+		// Load the acp_bbcode class
+		if (!class_exists('acp_bbcodes'))
+		{
+			include($this->phpbb_root_path . 'includes/acp/acp_bbcodes.' . $this->php_ext);
+		}
+		$bbcode_tool = new \acp_bbcodes();
+
+		foreach ($bbcode_data as $bbcode_name => $bbcode_array)
+		{
+			// Build the BBCodes
+			$data = $bbcode_tool->build_regexp($bbcode_array['bbcode_match'], $bbcode_array['bbcode_tpl']);
+
+			$bbcode_array += array(
+				'bbcode_tag'			=> $data['bbcode_tag'],
+				'first_pass_match'		=> $data['first_pass_match'],
+				'first_pass_replace'	=> $data['first_pass_replace'],
+				'second_pass_match'		=> $data['second_pass_match'],
+				'second_pass_replace'	=> $data['second_pass_replace']
+			);
+
+			$sql = 'SELECT bbcode_id
+				FROM ' . $this->table_prefix . "bbcodes
+				WHERE LOWER(bbcode_tag) = '" . strtolower($bbcode_name) . "'
+				OR LOWER(bbcode_tag) = '" . strtolower($bbcode_array['bbcode_tag']) . "'";
+			$result = $this->db->sql_query($sql);
+			$row_exists = $this->db->sql_fetchrow($result);
+			$this->db->sql_freeresult($result);
+
+			if ($row_exists)
+			{
+				// Update exisiting BBCode
+				$bbcode_id = $row_exists['bbcode_id'];
+
+				$sql = 'UPDATE ' . $this->table_prefix . 'bbcodes
+					SET ' . $this->db->sql_build_array('UPDATE', $bbcode_array) . '
+					WHERE bbcode_id = ' . $bbcode_id;
+				$this->db->sql_query($sql);
+			}
+			else
+			{
+				// Create new BBCode
+				$sql = 'SELECT MAX(bbcode_id) AS max_bbcode_id
+					FROM ' . $this->table_prefix . 'bbcodes';
+				$result = $this->db->sql_query($sql);
+				$row = $this->db->sql_fetchrow($result);
+				$this->db->sql_freeresult($result);
+
+				if ($row)
+				{
+					$bbcode_id = $row['max_bbcode_id'] + 1;
+
+					// Make sure it is greater than the core BBCode ids...
+					if ($bbcode_id <= NUM_CORE_BBCODES)
+					{
+						$bbcode_id = NUM_CORE_BBCODES + 1;
+					}
+				}
+				else
+				{
+					$bbcode_id = NUM_CORE_BBCODES + 1;
+				}
+
+				if ($bbcode_id <= BBCODE_LIMIT)
+				{
+					$bbcode_array['bbcode_id'] = (int) $bbcode_id;
+					$bbcode_array['display_on_posting'] = 1;
+
+					$this->db->sql_query('INSERT INTO ' . $this->table_prefix . 'bbcodes ' . $this->db->sql_build_array('INSERT', $bbcode_array));
+				}
+			}
+		}
+
+		// Resynchronize BBCodes
+		$manager = new \vse\abbc3\core\acp_manager($this->db, $this->request, $this->user);
+		$manager->resynchronize_bbcode_order();
+	}
+}
--- migrations/v310_m4_install_data.php
+++ migrations/v310_m4_install_data.php
@@ -33,89 +33,7 @@ class v310_m4_install_data extends \phpbb\db\migration\migration
 
 	public function install_abbc3_bbcodes()
 	{
-		// Load the acp_bbcode class
-		if (!class_exists('acp_bbcodes'))
-		{
-			include($this->phpbb_root_path . 'includes/acp/acp_bbcodes.' . $this->php_ext);
-		}
-		$bbcode_tool = new \acp_bbcodes();
-
-		$bbcode_data = $this->abbc3_bbcode_data();
-
-		foreach ($bbcode_data as $bbcode_name => $bbcode_array)
-		{
-			// Build the BBCodes
-			$data = $bbcode_tool->build_regexp($bbcode_array['bbcode_match'], $bbcode_array['bbcode_tpl']);
-
-			$bbcode_array += array(
-				'bbcode_tag'			=> $data['bbcode_tag'],
-				'first_pass_match'		=> $data['first_pass_match'],
-				'first_pass_replace'	=> $data['first_pass_replace'],
-				'second_pass_match'		=> $data['second_pass_match'],
-				'second_pass_replace'	=> $data['second_pass_replace']
-			);
-
-			$sql = 'SELECT bbcode_id
-				FROM ' . $this->table_prefix . "bbcodes
-				WHERE LOWER(bbcode_tag) = '" . strtolower($bbcode_name) . "'
-				OR LOWER(bbcode_tag) = '" . strtolower($bbcode_array['bbcode_tag']) . "'";
-			$result = $this->db->sql_query($sql);
-			$row_exists = $this->db->sql_fetchrow($result);
-			$this->db->sql_freeresult($result);
-
-			if ($row_exists)
-			{
-				// Update exisiting BBCode
-				$bbcode_id = $row_exists['bbcode_id'];
-
-				$sql = 'UPDATE ' . $this->table_prefix . 'bbcodes
-					SET ' . $this->db->sql_build_array('UPDATE', $bbcode_array) . '
-					WHERE bbcode_id = ' . $bbcode_id;
-				$this->db->sql_query($sql);
-			}
-			else
-			{
-				// Create new BBCode
-				$sql = 'SELECT MAX(bbcode_id) AS max_bbcode_id
-					FROM ' . $this->table_prefix . 'bbcodes';
-				$result = $this->db->sql_query($sql);
-				$row = $this->db->sql_fetchrow($result);
-				$this->db->sql_freeresult($result);
-
-				if ($row)
-				{
-					$bbcode_id = $row['max_bbcode_id'] + 1;
-
-					// Make sure it is greater than the core BBCode ids...
-					if ($bbcode_id <= NUM_CORE_BBCODES)
-					{
-						$bbcode_id = NUM_CORE_BBCODES + 1;
-					}
-				}
-				else
-				{
-					$bbcode_id = NUM_CORE_BBCODES + 1;
-				}
-
-				if ($bbcode_id <= BBCODE_LIMIT)
-				{
-					$bbcode_array['bbcode_id'] = (int) $bbcode_id;
-					$bbcode_array['display_on_posting'] = 1;
-
-					$this->db->sql_query('INSERT INTO ' . $this->table_prefix . 'bbcodes ' . $this->db->sql_build_array('INSERT', $bbcode_array));
-				}
-			}
-		}
-
-		// Resynchronize BBCodes
-		global $request, $user;
-		$bbcode_mgr = new \vse\abbc3\core\acp_manager($this->db, $request, $user);
-		$bbcode_mgr->resynchronize_bbcode_order();
-	}
-
-	public function abbc3_bbcode_data()
-	{
-		return array(
+		$bbcode_data = array(
 			'font=' => array(
 				'bbcode_helpline'	=> 'ABBC3_FONT_HELPLINE',
 				'bbcode_match'		=> '[font={INTTEXT}]{TEXT}[/font]',
@@ -233,5 +151,9 @@ class v310_m4_install_data extends \phpbb\db\migration\migration
 				'display_on_posting'=> 0,
 			),
 		);
+
+		global $request, $user;
+		$bbcode_manager = new \vse\abbc3\core\bbcodes_manager($this->db, $request, $user, $this->phpbb_root_path, $this->php_ext, $this->table_prefix);
+		$bbcode_manager->install_bbcodes($bbcode_data);
 	}
 }
