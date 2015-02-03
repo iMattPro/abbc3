4ab461108876d7f86b4ed81c8888fd931a306ceb
Matt Friedman <maf675@gmail.com>
2015-02-02 17:27:14 -0800

    install only if phpBB 3.1.3 or newer is found

---
+++ ext.php
@@ -0,0 +1,28 @@
+<?php
+/**
+*
+* Advanced BBCode Box 3.1
+*
+* @copyright (c) 2015 Matt Friedman
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace vse\abbc3;
+
+class ext extends \phpbb\extension\base
+{
+	/** @var string Require 3.1.3 due to throwing new exceptions
+	*               and using containerAware migration files.
+	*/
+	const PHPBB_VERSION = '3.1.3';
+
+	/**
+	* @inheritdoc
+	*/
+	function is_enableable()
+	{
+		$config = $this->container->get('config');
+		return version_compare($config['version'], self::PHPBB_VERSION, '>=');
+	}
+}
