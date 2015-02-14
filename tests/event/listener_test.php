afa68950c94cfc69e9c9086827f3ef26964e8153
Matt Friedman <maf675@gmail.com>
2015-02-14 09:45:45 -0800

    Update listener tests

--- tests/event/listener_test.php
+++ tests/event/listener_test.php
@@ -12,18 +12,45 @@ namespace vse\abbc3\tests\event;
 
 class listener_test extends \phpbb_test_case
 {
-	protected $bbcodes, $config, $controller_helper, $listener, $parser, $template, $user, $root_path, $ext_root_path, $bbvideo_width, $bbvideo_height;
+	/** @var \PHPUnit_Framework_MockObject_MockObject */
+	protected $bbcodes;
+
+	/** @var \phpbb\config\config */
+	protected $config;
+
+	/** @var \PHPUnit_Framework_MockObject_MockObject */
+	protected $controller_helper;
+
+	/** @var \vse\abbc3\event\listener */
+	protected $listener;
+
+	/** @var \PHPUnit_Framework_MockObject_MockObject */
+	protected $parser;
+
+	/** @var \PHPUnit_Framework_MockObject_MockObject */
+	protected $template;
+
+	/** @var \phpbb\user */
+	protected $user;
+
+	/** @var string */
+	protected $root_path;
+
+	/** @var string */
+	protected $ext_root_path;
+
+	/** @var string */
+	protected $bbvideo_width;
+
+	/** @var string */
+	protected $bbvideo_height;
 
 	public function setUp()
 	{
 		parent::setUp();
 
-		global $phpbb_dispatcher, $phpbb_root_path, $phpEx;
+		global $phpbb_root_path;
 
-		// Mock some global classes that may be called during code execution
-		$phpbb_dispatcher = new \phpbb_mock_event_dispatcher();
-
-		// Load/Mock classes required by the event listener class
 		$this->parser = $this->getMockBuilder('\vse\abbc3\core\bbcodes_parser')
 			->disableOriginalConstructor()
 			->getMock();
@@ -37,24 +64,15 @@ class listener_test extends \phpbb_test_case
 		$this->user = $this->getMock('\phpbb\user', array(), array('\phpbb\datetime'));
 		$this->user->data['username'] = 'admin';
 
-		$request = new \phpbb_mock_request();
-		$request->overwrite('SCRIPT_NAME', 'app.php', \phpbb\request\request_interface::SERVER);
-		$request->overwrite('SCRIPT_FILENAME', 'app.php', \phpbb\request\request_interface::SERVER);
-		$request->overwrite('REQUEST_URI', 'app.php', \phpbb\request\request_interface::SERVER);
+		$this->controller_helper = $this->getMockBuilder('\phpbb\controller\helper')
+			->disableOriginalConstructor()
+			->getMock();
+		$this->controller_helper->expects($this->any())
+			->method('route')
+			->willReturnCallback(function ($route, array $params = array()) {
+				return $route . '#' . serialize($params);
+			});
 
-		$this->controller_helper = new \phpbb_mock_controller_helper(
-			$this->template,
-			$this->user,
-			$this->config,
-			new \phpbb\controller\provider(),
-			new \phpbb_mock_extension_manager($phpbb_root_path),
-			new \phpbb\symfony_request($request),
-			$request,
-			new \phpbb\filesystem(),
-			'',
-			$phpEx,
-			dirname(__FILE__) . '/../../'
-		);
 		$this->root_path = $phpbb_root_path;
 		$this->ext_root_path = 'ext/vse/abbc3/';
 		$this->bbvideo_width = 560;
@@ -211,7 +229,7 @@ class listener_test extends \phpbb_test_case
 				'ABBC3_BBCODE_ICONS' 		=> $this->ext_root_path . 'images/icons',
 				'ABBC3_BBVIDEO_HEIGHT'		=> $this->bbvideo_height,
 				'ABBC3_BBVIDEO_WIDTH'		=> $this->bbvideo_width,
-				'UA_ABBC3_BBVIDEO_WIZARD'	=> 'app.php/wizard/bbcode/bbvideo',
+				'UA_ABBC3_BBVIDEO_WIZARD'	=> 'vse_abbc3_bbcode_wizard#a:1:{s:4:"mode";s:7:"bbvideo";}',
 			));
 
 		$dispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
