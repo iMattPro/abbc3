430e760b751adb86d16914deba85798c2d87d410
Matt Friedman <maf675@gmail.com>
2015-03-20 18:49:47 -0700

    Fix coverage

--- travis/run-tests.sh
+++ travis/run-tests.sh
@@ -14,7 +14,7 @@ EXTNAME=$3
 
 if [ "$TRAVIS_PHP_VERSION" == "5.3.3" -a "$DB" == "mysqli" ]
 then
-	phpBB/vendor/bin/phpunit --configuration phpBB/ext/$EXTNAME/travis/phpunit-$DB-travis.xml --bootstrap ./tests/bootstrap.php --coverage-text --coverage-clover=coverage.clover
+	phpBB/vendor/bin/phpunit --configuration phpBB/ext/$EXTNAME/travis/phpunit-$DB-travis.xml --bootstrap ./tests/bootstrap.php --coverage-clover=coverage.clover
 else
 	phpBB/vendor/bin/phpunit --configuration phpBB/ext/$EXTNAME/travis/phpunit-$DB-travis.xml --bootstrap ./tests/bootstrap.php
 fi
--- travis/send-coverage.sh
+++ travis/send-coverage.sh
@@ -14,6 +14,6 @@ TRAVIS_PHP_VERSION=$2
 if [ "$TRAVIS_PHP_VERSION" == "5.3.3" -a "$DB" == "mysqli" ]
 then
     cd ../VSEphpbb/abbc3
-    - wget https://scrutinizer-ci.com/ocular.phar
-    - php ocular.phar code-coverage:upload --format=php-clover ../../phpBB3/coverage.clover
+    wget https://scrutinizer-ci.com/ocular.phar
+    php ocular.phar code-coverage:upload --format=php-clover ../../phpBB3/coverage.clover
 fi
