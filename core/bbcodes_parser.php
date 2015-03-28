dc9b373667e1effd91e9f7aec216735ea672615a
Matt Friedman <maf675@gmail.com>
2015-03-28 12:29:34 -0700

    Prepare old bbvideo tags for s9e/textformatter

--- core/bbcodes_parser.php
+++ core/bbcodes_parser.php
@@ -46,11 +46,28 @@ class bbcodes_parser
 	 * @param string $uid  The BBCode UID
 	 * @return string The parsed text
 	 * @access public
+	 *
+	 * @deprecated 3.2.0. Provides bc for phpBB 3.1.x.
 	 */
 	public function pre_parse_bbcodes($text, $uid)
 	{
 		// bbvideo BBCodes (convert from older ABBC3 installations)
-		$text = preg_replace_callback('#\[(bbvideo)[\s]?([0-9,]+)?:(' . $uid . ')\]([^[]+)\[/\1:\3\]#is', array($this, 'bbvideo_pass'), $text);
+		$text = preg_replace_callback('#\[(bbvideo)[\s]?([0-9,]+)?(:' . $uid . ')\]([^[]+)\[/\1\3\]#is', array($this, 'bbvideo_pass'), $text);
+
+		return $text;
+	}
+
+	/**
+	 * Pre-Parser for special custom BBCodes created by ABBC3
+	 *
+	 * @param string $text The text to parse
+	 * @return string The parsed text
+	 * @access public
+	 */
+	public function s9e_pre_parse_bbcodes($text)
+	{
+		// bbvideo BBCodes (convert from older ABBC3 installations)
+		$text = preg_replace_callback('#\[(bbvideo)[\s]?([0-9,]+)?(:[^\]]+)?\]([^[]+)\[\/\1(:[^\]]+)?\]#is', array($this, 'bbvideo_pass'), $text);
 
 		return $text;
 	}
@@ -79,7 +96,7 @@ class bbcodes_parser
 	 */
 	protected function bbvideo_pass($matches)
 	{
-		return (!empty($matches[2])) ? "[bbvideo=$matches[2]:$matches[3]]$matches[4][/bbvideo:$matches[3]]" : "[bbvideo={$this->bbvideo_width},{$this->bbvideo_height}:$matches[3]]$matches[4][/bbvideo:$matches[3]]";
+		return (!empty($matches[2])) ? "[bbvideo=$matches[2]$matches[3]]$matches[4][/bbvideo$matches[3]]" : "[bbvideo={$this->bbvideo_width},{$this->bbvideo_height}$matches[3]]$matches[4][/bbvideo$matches[3]]";
 	}
 
 	/**
