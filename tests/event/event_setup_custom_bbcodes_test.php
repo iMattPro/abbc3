cb3dea7c7c15e57a37e9698fe89869c791f17083
Matt Friedman <maf675@gmail.com>
2015-04-23 11:45:52 -0700

    Fix test

--- tests/event/event_setup_custom_bbcodes_test.php
+++ tests/event/event_setup_custom_bbcodes_test.php
@@ -28,6 +28,7 @@ class event_setup_custom_bbcodes_test extends event_listener_base
 				'ABBC3_BBVIDEO_HEIGHT'		=> $this->bbvideo_height,
 				'ABBC3_BBVIDEO_WIDTH'		=> $this->bbvideo_width,
 				'UA_ABBC3_BBVIDEO_WIZARD'	=> 'vse_abbc3_bbcode_wizard#a:1:{s:4:"mode";s:7:"bbvideo";}',
+				'UA_ABBC3_URL_WIZARD'		=> 'vse_abbc3_bbcode_wizard#a:1:{s:4:"mode";s:3:"url";}',
 			));
 
 		$dispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
