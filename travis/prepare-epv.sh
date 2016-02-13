b2013e857b5daf0028af4a1d507757b2a27ad5f8
Matt Friedman <maf675@gmail.com>
2016-02-13 09:07:08 -0800

    Update EPV implementation

---
+++ travis/prepare-epv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Advanced BBCode Box
+#
+# @copyright (c) 2016 Matt Friedman
+# @license GNU General Public License, version 2 (GPL-2.0)
+#
+set -e
+set -x
+
+DB=$1
+TRAVIS_PHP_VERSION=$2
+
+if [ "$DB" == "mysqli" -a "$TRAVIS_PHP_VERSION" == "5.3.3" ]
+then
+	cd phpBB
+	composer require phpbb/epv:dev-master --dev --no-interaction --prefer-source
+	cd ../
+fi
