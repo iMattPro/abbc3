23678856586ed873fb4903aedb57e4708369fbab
Matt Friedman <maf675@gmail.com>
2017-07-26 00:15:20 -0700

    Update test env

--- travis/prepare-epv.sh
+++ travis/prepare-epv.sh
@@ -13,6 +13,7 @@ NOTESTS=$1
 if [ "$NOTESTS" == "1" ]
 then
 	cd phpBB
-	composer require phpbb/epv:dev-master --dev --no-interaction
+	composer remove sami/sami --dev --no-interaction
+	composer require phpbb/epv:dev-master --dev --no-interaction --ignore-platform-reqs
 	cd ../
 fi
