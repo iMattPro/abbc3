9edec3ae61cea093a4d4288b3d9a7bc7f2a890f3
Matt Friedman <maf675@gmail.com>
2019-01-09 17:47:24 -0800

    New PHP7 Stuff

--- controller/wizard.php
+++ controller/wizard.php
@@ -109,7 +109,7 @@ class wizard
 
 		$this->template->assign_vars(array(
 			'ABBC3_BBVIDEO_SITES'	=> $bbvideo_sites,
-			'ABBC3_BBVIDEO_LINK_EX'	=> isset($bbvideo_sites[self::BBVIDEO_DEFAULT]) ? $bbvideo_sites[self::BBVIDEO_DEFAULT] : '',
+			'ABBC3_BBVIDEO_LINK_EX'	=> $bbvideo_sites[self::BBVIDEO_DEFAULT] ?? '',
 			'ABBC3_BBVIDEO_DEFAULT'	=> self::BBVIDEO_DEFAULT,
 		));
 	}
