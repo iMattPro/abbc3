aab127163d11aa03c53b94ad37abfe1c20fc770d
Matt Friedman <maf675@gmail.com>
2020-09-10 16:54:03 -0700

    remove sami command

--- travis/prepare-epv.sh
+++ travis/prepare-epv.sh
@@ -13,7 +13,6 @@ NOTESTS=$1
 if [ "$NOTESTS" == "1" ]
 then
 	cd phpBB
-	composer remove sami/sami --dev --no-interaction
 	composer require phpbb/epv:dev-master --dev --no-interaction --ignore-platform-reqs
 	cd ../
 fi
