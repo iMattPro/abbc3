eb3b0f940059eb294fabb0615781bf25e5fe08ec
phpBB España <thekuka@phpbb-es.com>
2020-09-13 09:35:10 +0200

    Spanish update
    Spanish translations update

---
+++ language/es/info_acp_abbc3.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * Advanced BBCode Box [Spanish - Formal Honorifics]
+ *
+ * @copyright (c) 2020 Matt Friedman
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'ACP_ABBC3_MODULE'		=> 'Caja de BBCode Avanzado',
+	'ACP_ABBC3_SETTINGS'	=> 'Ajustes',
+]);
