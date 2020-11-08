94f712eb293bc963d99f0d32bc455f8154c69671
Matt Friedman <maf675@gmail.com>
2020-11-07 16:56:21 -0800

    Update Chinese

--- language/zh_cmn_hans/info_acp_abbc3.php
+++ language/zh_cmn_hans/info_acp_abbc3.php
@@ -1,9 +1,10 @@
 <?php
 /**
  *
- * Advanced BBCode Box
+ * Advanced BBCode Box [Simplified Chinese]
+ * @简体中文语言　David Yin <https://www.phpbbchinese.com/>
  *
- * @copyright (c) 2020 Matt Friedman
+ * @copyright (c) 2013 Matt Friedman
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
  */
@@ -20,6 +21,6 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, [
 	'ACP_ABBC3_MODULE'		=> 'Advanced BBCode Box',
-	'ACP_ABBC3_SETTINGS'	=> 'Settings',
-	'LOG_ABBC3_ENABLE_FAIL'	=> '<strong>Advanced BBCode Box was unable to create the directory:</strong><br>%s',
+	'ACP_ABBC3_SETTINGS'	=> '设置',
+	'LOG_ABBC3_ENABLE_FAIL'	=> '<strong>Advanced BBCode Box 无法创建以下目录：</strong><br>%s',
 ]);
