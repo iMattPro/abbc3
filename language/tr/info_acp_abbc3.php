e2dd8ad52e93ccda59e2bd3a8ccc0c0fb2089abf
Matt Friedman <maf675@gmail.com>
2021-01-19 16:00:43 -0800

    Turkish updates

--- language/tr/info_acp_abbc3.php
+++ language/tr/info_acp_abbc3.php
@@ -1,7 +1,8 @@
 <?php
 /**
  *
- * Advanced BBCode Box
+ * Advanced BBCode Box [Turkish]
+ * Turkish translation by ESQARE (https://www.phpbbturkey.com)
  *
  * @copyright (c) 2020 Matt Friedman
  * @license GNU General Public License, version 2 (GPL-2.0)
@@ -20,6 +21,6 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, [
 	'ACP_ABBC3_MODULE'		=> 'Advanced BBCode Box',
-	'ACP_ABBC3_SETTINGS'	=> 'Settings',
-	'LOG_ABBC3_ENABLE_FAIL'	=> '<strong>Advanced BBCode Box was unable to create the directory:</strong><br>%s',
+	'ACP_ABBC3_SETTINGS'	=> 'Ayarlar',
+	'LOG_ABBC3_ENABLE_FAIL'	=> '<strong>Advanced BBCode Box dizin oluşturamadı:</strong><br>%s',
 ]);
