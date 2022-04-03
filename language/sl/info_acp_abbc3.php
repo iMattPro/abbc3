407f071fec31f0e5f41c02ef56cd3ee65cfdca41
max-ima <96241406+max-ima@users.noreply.github.com>
2022-04-03 15:30:51 +0200

    Slovenian
    Slovenian translation

---
+++ language/sl/info_acp_abbc3.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * Advanced BBCode Box
+ *
+ * @copyright (c) 2020 Matt Friedman
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ * Slovenian Translation - Marko K.(max, max-ima,...)
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'ACP_ABBC3_MODULE'		=> 'Napredno polje BBKode/<br />(Advanced BBCode Box)',
+	'ACP_ABBC3_SETTINGS'	=> 'Nastavitve',
+	'LOG_ABBC3_ENABLE_FAIL'	=> '<strong>Napredno polje BBKode ni mogel ustvariti imenika:</strong><br>%s',
+]);
