098b2e958767d916168642058d1b173caa0cb785
Matt Friedman <maf675@gmail.com>
2025-09-02 08:42:05 -0700

    Add functional testing of hidden bbcode

--- tests/functional/bbcode_posting_test.php
+++ tests/functional/bbcode_posting_test.php
@@ -34,4 +34,24 @@ class bbcode_posting_test extends \phpbb_functional_test_case
 		self::assertStringContainsString('background-color:yellow', $crawler->filter('div.content > span')->attr('style'));
 		self::assertStringContainsString('This is a test topic posted by the testing framework.', $crawler->filter('html')->text());
 	}
+
+	/**
+	 * Test hidden bbcode
+	 */
+	public function test_hidden_bbcode()
+	{
+		$this->login();
+
+		$post = $this->create_topic(2, 'Test Topic Hidden', '[hidden]This is hidden text posted by the testing framework.[/hidden]');
+
+		$crawler = self::request('GET', "viewtopic.php?t={$post['topic_id']}&sid=$this->sid");
+		self::assertStringContainsString('Hidden content (visible to members)', $crawler->filter('.content > .hc-box')->text());
+		self::assertStringContainsString('This is hidden text posted by the testing framework.', $crawler->filter('.content > .hc-box')->text());
+
+		$this->logout();
+
+		$crawler = self::request('GET', "viewtopic.php?t={$post['topic_id']}");
+		self::assertStringContainsString('Log in or register to view this post.', $crawler->filter('.content > .hc-box')->text());
+		self::assertStringNotContainsString('This is hidden text posted by the testing framework.', $crawler->filter('.content > .hc-box')->text());
+	}
 }
