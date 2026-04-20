0e7a63a76727f84270f0713983fb6ef0aeddbd0c
Matt Friedman <maf675@gmail.com>
2026-04-20 07:11:39 -0700

    Remove emoji from migration

--- migrations/v339_m19_update_bbcodes.php
+++ migrations/v339_m19_update_bbcodes.php
@@ -19,7 +19,8 @@ class v339_m19_update_bbcodes extends bbcodes_migration_base
 	{
 		return [
 			'\vse\abbc3\migrations\v310_m4_install_data',
-			'\vse\abbc3\migrations\v335_m16_update_bbcodes'];
+			'\vse\abbc3\migrations\v335_m16_update_bbcodes'
+		];
 	}
 
 	/**
@@ -39,7 +40,7 @@ class v339_m19_update_bbcodes extends bbcodes_migration_base
 		'offtopic' => [
 			'bbcode_helpline'	=> 'ABBC3_OFFTOPIC_HELPLINE',
 			'bbcode_match'		=> '[offtopic]{TEXT}[/offtopic]',
-			'bbcode_tpl'		=> '<div class="abbc3-off-topic" style="display:flex;align-items:stretch;max-width:100%;margin:1em 0;border-radius:8px;overflow:hidden;color:#333;border:1px solid #ddd;"><div style="background-color:#6c757d;color:#fff;font-weight:bold;font-size:1.8em;display:flex;align-items:center;justify-content:center;padding:10px;width:60px;">ℹ</div><div style="flex:1;display:flex;flex-direction:column;"><div style="background-color:#f1f3f5;font-weight:bold;padding:8px 12px;border-bottom:1px solid #ddd;">{L_ABBC3_OFFTOPIC}</div><div style="background-color:#fff;padding:12px 12px 14px 12px;line-height:1.5;">{TEXT}</div></div></div>',
+			'bbcode_tpl'		=> '<div class="abbc3-off-topic" style="display:flex;align-items:stretch;max-width:100%;margin:1em 0;border-radius:8px;overflow:hidden;color:#333;border:1px solid #ddd;"><div style="background-color:#6c757d;color:#fff;font-weight:bold;font-size:1.8em;display:flex;align-items:center;justify-content:center;padding:10px;width:60px;">&#8505;</div><div style="flex:1;display:flex;flex-direction:column;"><div style="background-color:#f1f3f5;font-weight:bold;padding:8px 12px;border-bottom:1px solid #ddd;">{L_ABBC3_OFFTOPIC}</div><div style="background-color:#fff;padding:12px 12px 14px 12px;line-height:1.5;">{TEXT}</div></div></div>',
 		],
 		'mod=' => [
 			'bbcode_helpline'	=> 'ABBC3_MOD_HELPLINE',
